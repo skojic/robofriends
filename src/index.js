cbba04c1ecaa28bad008d8c8303cdd19bc9a050e
Srdjan Kojic <srdjan.m.kojic@gmail.com>
2018-07-22 08:47:25 +0200

    Adding parent App.js component and making changes in the index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from'./CardList';
+import App from'./App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import { robots } from './robots';
