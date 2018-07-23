00197ca0d90e2c6a65c9260265c569af90907257
Srdjan Kojic <srdjan.m.kojic@gmail.com>
2018-07-23 17:44:51 +0200

    changing scroll element

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll'
 
 
 class App extends Component {
@@ -34,9 +35,9 @@ class App extends Component {
 			<div className='tc'>
 				<h1 className='f1'>Robofriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<scroll>
+				<Scroll>
 				<CardList robots={filteredRobots} />
-				</scroll>
+				</Scroll>
 			</div>
 		);
 
