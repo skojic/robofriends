8c034ce0704a1f95558f36dc4244aff484584a42
skojic <srdjan.m.kojic@gmail.com>
2018-09-13 15:39:06 +0200

    adding styles and fonts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    this.setState({ robots: robots })
   }
 
   onSearchChange = (event) => {
@@ -23,7 +29,7 @@ class App extends Component {
 
     return (
       <div className='tc'>
-        <h1>Cat Friends</h1>
+        <h1 className='f1'>Cat Friends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
       </div>
