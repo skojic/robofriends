31ee6afb569ed34cffbd098cc89b219a52ab069f
Srdjan Kojic <srdjan.m.kojic@gmail.com>
2018-12-05 23:00:41 +0100

    Updates

---
+++ precache-manifest.692b3e74241fd0559b966e32a0435ba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "ce9b94d543030cca2650",
+    "url": "/robofriends/static/js/main.ce9b94d5.chunk.js"
+  },
+  {
+    "revision": "77058e1da21dfb7d2c46",
+    "url": "/robofriends/static/js/1.77058e1d.chunk.js"
+  },
+  {
+    "revision": "ce9b94d543030cca2650",
+    "url": "/robofriends/static/css/main.77f72c6b.chunk.css"
+  },
+  {
+    "revision": "77058e1da21dfb7d2c46",
+    "url": "/robofriends/static/css/1.3ac4a465.chunk.css"
+  },
+  {
+    "revision": "f3c2669b22ca8279dccc83ce56d9d0e7",
+    "url": "/robofriends/index.html"
+  }
+];
